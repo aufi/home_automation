e16da2cbfe6a198afc5946df5c3cbc2b6064369c
Marek Aufart <aufi.cz@gmail.com>
2018-04-01 21:53:33 +0200

    remove not needed temp setup

--- haab/lib/haab/controls/inputs/temperature.rb
+++ haab/lib/haab/controls/inputs/temperature.rb
@@ -3,9 +3,6 @@ module Haab
     module Inputs
       class Temperature < Haab::Controls::InputW1
         def run
-          `gpio -g mode #{pin} down`
-          previous_input = 0
-
           loop do
             temp = File.read("/sys/bus/w1/devices/#{w1_device}/w1_slave").split("=").last.to_f / 1000
             fire_action 'obyvak/temp', temp, true
