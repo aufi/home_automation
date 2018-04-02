94cd76bba8c2b67240e8f5789dcb3fa3f3f1d5a5
Marek Aufart <aufi.cz@gmail.com>
2018-04-02 06:59:27 +0200

    require classes for inputs first

--- haab/lib/haab.rb
+++ haab/lib/haab.rb
@@ -1,3 +1,7 @@
+# abstract classes for controls first (ruby 2.5 problem)
+Dir['./lib/haab/controls/*.rb'].each{ |f| require f }
+
+# load everything
 Dir['./lib/**/*.rb'].each{ |f| require f }
 
 module Haab
